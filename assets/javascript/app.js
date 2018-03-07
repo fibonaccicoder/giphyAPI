7cfeadfc22061937fd8afe642f7aac21bb694e67
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-03-06 23:41:55 -0700

    fixed gif loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,80 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var reactions = ["anger", "love", "happy", "cute", "cuddle", "annoyed", "irritated", "hug", "kiss", "sad"];
-var APIKey = "s3ZDUTZMiKXdgbfp7sj2bUGFqMfXTpUS";
-var queryURL = "https://api.giphy.com/v1/gifs/search?&q=" + reaction + "&lang=en&api_key=" + APIKey + "&limit=15";
+  var reactions = ["anger", "love", "happy", "cute", "cuddle", "annoyed", "irritated", "hug", "kiss", "sad"];
 
 
-//creates buttons from items in reactions array
- function createButtons(){
-     for(var i =0; i < reactions.length; i++){
+  //creates buttons from items in reactions array
+  function createButtons() {
+    reaction = $("#button-space").val("");
+    $("#button-space").empty();
+
+    for (var i = 0; i < reactions.length; i++) {
       var gifButton = $("<button>");
       gifButton.addClass("buttons btn btn-standard");
-      gifButton.attr("data-name", reaction);
+      gifButton.attr("data-name", reactions[i]);
       gifButton.text(reactions[i]);
       $('#button-space').append(gifButton);
     }
+  }
 
- //settings for submit button, prevents page reload on submit click
- //adds new topics to reactions array to be rendered into new buttons on page
-    $("#add-subject").on("click", function(event) {
+  //settings for submit button, prevents page reload on submit click
+  //adds new topics to reactions array to be rendered into new buttons on page
+  $("#add-subject").on("click", function (event) {
     event.preventDefault();
     var newButton = $("#search-term").val().trim();
     reactions.push(newButton);
-    $("#search-term").val(" ");
     createButtons();
-    });
+  });
 
 
-function displayGifs(){
-  //inputs search term into query url to get specific gifs
-  var reaction = $(this).val("#search-term");
-//ajax call for json object
-	 $.ajax({
+  function displayGifs() {
+    //inputs search term into query url to get specific gifs
+    var reaction = $(this).data("name");
+    var APIKey = "s3ZDUTZMiKXdgbfp7sj2bUGFqMfXTpUS";
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + reaction + "&limit=15&offset=0&lang=en";
+
+    //ajax call for json object
+    $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-      console.log(queryURL);
-      console.log(response);
-//empty gif div from previous call
-      $("#gif-view").empty();
-//gives attributes for variables for image, gif, and rating
-      var animatedGif = $("<img>").attr("src", response.data[i].images.fixed_height.url);
-      var rating = $("<p>").text(response.data[i].rating);
-//appends items to gif-view div
-      $("#gif-view").append(animatedGif, rating);
-    })
-}
+    }).then(function (response) {
 
-//may or many not pause gifs when clicked on lol
-  $('.img').on("click", function(){
-        var src = $(this).attr('src')
-        if(src.includes('_s.gif')){
-          $(this).attr('src', src.replace('_s.gif','.gif'));
-           }
-        else{
-          $(this).attr('src', src.replace('.gif','_s.gif'));
-         }
-       })
-     };
+      console.log(response);
+      // empty gif div from previous call
+      $("#gif-view").empty();
+
+      for (var i = 0; i < response.data.length; i++) {
+        //gives attributes for variables for image, gif, and rating
+        var animatedGif = $("<img/>").attr("src", response.data[i].images.fixed_height_still.url);
+        var rating = $("<p>").text(response.data[i].rating);
+        $("#gif-view").append(animatedGif, rating);
+      }
+    })
+    console.log(queryURL);
   }
 
-//function call
+  //pause thos mofos
+  $(".gif").on("click", function () {
 
- $(document).on("click", ".buttons", displayGifs);
-    createButtons();
 
-}
+    var state = $(this).attr("data-state");
 
-//dynamically create buttons
-//create buttons when added to search box
-//pull up unique api according to the data attributes when each specific button is clicked
-//push new search to topics array to be displayed when clicked on 
-//pause gifs until clicked on 
-//pull up gifs, use parameters q, limit and rating
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+
+    }
+  })
+
+
+  //function call
+
+  $(document).on("click", ".buttons", displayGifs);
+
+
+  createButtons();
+})
